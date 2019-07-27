d5cba7bdc3c3c7d23f34e09674b0a16fc077c744
aman151280 <kumaraman2006@gmail.com>
2019-07-27 15:48:20 +0530

    root package define for Applipcation class.

--- src/main/java/com/employee/demo/EmployeeApplication.java
+++ src/main/java/com/employee/demo/EmployeeApplication.java
@@ -1,4 +1,4 @@
-package com.employee.demo.config;
+package com.employee.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
